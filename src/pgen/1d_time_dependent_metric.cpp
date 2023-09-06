2dd8d1deaf32d5570dad5772534b3247d6c558ff
Sean Ressler <smressle@berkeley.edu>
2023-09-05 23:46:36 -0400

    iave

--- src/pgen/1d_time_dependent_metric.cpp
+++ src/pgen/1d_time_dependent_metric.cpp
@@ -665,7 +665,16 @@ void CustomInnerX1(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &prim,
 
         Real t;
         Real xprime = pco->x1v(is-i);
-        Real tprime = pmb->pmy_mesh->metric_time;
+
+        Real tprime;
+        
+        if (std::abs((time - (pmb->pmy_mesh->time + dt))) < 1e-2*dt) {
+          tprime = time;
+          fprintf('end of stage in boundary!! \n t: %g old_t: %g dt: %g t_end_step: %g \n',time,pmb->pmy_mesh->time,dt, pmb->pmy_mesh->time + dt)
+        }
+        else{
+          tprime = pmb->pmy_mesh->metric_time;
+        }
         get_t_from_prime(tprime,xprime,pco->x2v(j), pco->x3v(k),&t);
 
         Real v = v_func(t);
@@ -759,7 +768,16 @@ void CustomOuterX1(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &prim,
         Real uz = 0.0;
 
         Real t;
-        Real tprime = pmb->pmy_mesh->metric_time;
+
+        Real tprime;
+        
+        if (std::abs((time - (pmb->pmy_mesh->time + dt))) < 1e-2*dt) {
+          tprime = time;
+          fprintf('end of stage in boundary!! \n t: %g old_t: %g dt: %g t_end_step: %g \n',time,pmb->pmy_mesh->time,dt, pmb->pmy_mesh->time + dt)
+        }
+        else{
+          tprime = pmb->pmy_mesh->metric_time;
+        }
         Real xprime = pco->x1v(ie+i);
         get_t_from_prime(tprime,xprime,pco->x2v(j), pco->x3v(k),&t);
 
