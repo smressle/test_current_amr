472c91a3bcbbc73a2cdcbd54ae046cc7e458caa6
Sean Ressler <smressle@berkeley.edu>
2023-09-23 23:58:35 -0400

    iave

--- src/pgen/moving_bh_constant_velocity_boosted.cpp
+++ src/pgen/moving_bh_constant_velocity_boosted.cpp
@@ -2148,16 +2148,16 @@ void metric_for_derivatives(Real t, Real x1, Real x2, Real x3, AthenaArray<Real>
   // Lambda(I23) = (       (Lorentz - 1.0) * ny * nz );
   // Lambda(I33) = ( 1.0 + (Lorentz - 1.0) * nz * nz );
 
-  Lambda(I00) =  1.0;
+  Lambda(I00) =  Lorentz;
   Lambda(I01) = 0.0;
   Lambda(I02) = 0.0;
-  Lambda(I03) = 0.0;
+  Lambda(I03) = -Lorentz * v_bh2;
   Lambda(I11) = 1.0;
   Lambda(I12) = 0.0; 
   Lambda(I13) = 0.0;
   Lambda(I22) = 1.0; 
   Lambda(I23) = 0.0;
-  Lambda(I33) = 1.0;
+  Lambda(I33) = Lorentz;
 
 
 
