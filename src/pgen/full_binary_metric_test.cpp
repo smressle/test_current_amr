c606bdb67f5b7783e4acd9ca79316b4183e1d790
Sean Ressler <smressle@berkeley.edu>
2024-04-09 13:12:32 -0400

    ivave

--- src/pgen/full_binary_metric_test.cpp
+++ src/pgen/full_binary_metric_test.cpp
@@ -1782,12 +1782,12 @@ void apply_inner_boundary_condition(MeshBlock *pmb,AthenaArray<Real> &prim,Athen
 
 
               Real git_ui = g01_ *u1prime + g02_ *u2prime + g03_ *u3prime;
-              Real gij_ui_uj = g(I11,i)*u1prime*u1prime + 2.0*g(I12,i)*u1prime*u2prprime + 2.0*g(I13,i)*u1prime*u3prime
+              Real gij_ui_uj = g(I11,i)*u1prime*u1prime + 2.0*g(I12,i)*u1prime*u2prime + 2.0*g(I13,i)*u1prime*u3prime
                        + g(I22,i)*u2prime*u2prime + 2.0*g(I23,i)*u2prime*u3prime
                        + g(I33,i)*u3prime*u3prime;
               Real a_const = g00_ -2.0*g00_*SQR(u0prime) + SQR(g00_*u0prime) * gij_ui_uj/SQR(git_ui);
               Real b_const = 2.0 * g00_*u0prime * gij_ui_uj/SQR(git_ui) - 2.0*u0prime;
-              Real c_const = (gij_ui_uj/SQR(git_ui) + 1.0)
+              Real c_const = (gij_ui_uj/SQR(git_ui) + 1.0);
 
               Real A_const = (- b_const + std::sqr( SQR(b_const) - 4.0 * a_const*c_const ) )/ (2*a_const);
               Real B_const = -1.0 / (git_ui) * (1.0 + A_const * g00_ * u0prime);
