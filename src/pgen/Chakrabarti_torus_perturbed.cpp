c8ac23adb2d2d5c5ab9f80018a96353e14cd0a5a
Sean Ressler <smressle@berkeley.edu>
2025-02-14 13:47:32 -0500

    iave

--- src/pgen/Chakrabarti_torus_perturbed.cpp
+++ src/pgen/Chakrabarti_torus_perturbed.cpp
@@ -3404,7 +3404,7 @@ void Binary_BH_Metric(Real t, Real x1, Real x2, Real x3,
       Real dl0_dx3 = 0.0;
 
       if (std::isnan(f) || std::isnan(r) || std::isnan(sqrt_term) || std::isnan (df_dx1) || std::isnan(df_dx2)){
-        fprintf(stderr,"ISNAN in metric\n x y y: %g %g %g r: %g \n ijk: %d %d %d \n xyz_old: %g %g %g \n th: %g phi: %g \n",x,y,z,r,i,j,k,x_old,y_old,z_old,th,phi);
+        fprintf(stderr,"ISNAN in metric\n x y y: %g %g %g r: %g \n xyz_old: %g %g %g \n th: %g phi: %g \n",x,y,z,r,x_old,y_old,z_old,th,phi);
         exit(0);
       }
 
