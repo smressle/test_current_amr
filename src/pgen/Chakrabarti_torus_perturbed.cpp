962dc3734578369b7a4f3e039513a5a7b2571391
Sean Ressler <smressle@berkeley.edu>
2025-03-08 00:12:58 -0500

    save

--- src/pgen/Chakrabarti_torus_perturbed.cpp
+++ src/pgen/Chakrabarti_torus_perturbed.cpp
@@ -2298,11 +2298,11 @@ void NobleCooling(MeshBlock *pmb, const Real time, const Real dt,
         cons(IM3,k,j,i) += -dt * L_cool * u_3;
 
         Real ug_frac = dt * L_cool/ug;
-        if ((Y>100 || ug_frac>0.1) && L_cool>0 && radius<10.0){
-          fprintf(stderr,"High Y!  at xyz: %g %g %g \n r th ph: %g %g %g \n Y, ug_frac: %g %g ",
-            pmb->pcoord->x1v(i), pmb->pcoord->x2v(j), pmb->pcoord->x3v(k),radius,theta,phi,
-            Y,ug_frac );
-        }
+        // if ((Y>100 || ug_frac>0.1) && L_cool>0 && radius<10.0){
+        //   fprintf(stderr,"High Y!  at xyz: %g %g %g \n r th ph: %g %g %g \n Y, ug_frac: %g %g ",
+        //     pmb->pcoord->x1v(i), pmb->pcoord->x2v(j), pmb->pcoord->x3v(k),radius,theta,phi,
+        //     Y,ug_frac );
+        // }
 
         pmb->user_out_var(0,k,j,i) = L_cool;
         pmb->user_out_var(1,k,j,i) = Target_Temperature;
