78d6f2c1b4a2134018e81567e9ede4fa8712485b
Sean Ressler <smressle@berkeley.edu>
2025-03-13 14:14:20 -0400

    save

--- src/pgen/Chakrabarti_torus_perturbed.cpp
+++ src/pgen/Chakrabarti_torus_perturbed.cpp
@@ -2373,7 +2373,7 @@ void NobleCoolingPrimitive(MeshBlock *pmb, const Real time, const Real dt,
         Real L_cool = Omega * ug * std::sqrt( Y-1.0 +  std::fabs(Y-1.0) );
         if (L_cool<0) L_cool = 0.0;
 
-        if (radius<6.0) L_cool = 0.0
+        if (radius<6.0) L_cool = 0.0;
 
 
           // Calculate normal frame Lorentz factor
