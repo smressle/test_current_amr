67844829b55719174fdec1c7bed22dc2f2c3e1b0
Sean Ressler <smressle@berkeley.edu>
2025-06-28 23:42:08 -0400

    aivs

--- src/pgen/Chakrabarti_torus_perturbed_orbit_file.cpp
+++ src/pgen/Chakrabarti_torus_perturbed_orbit_file.cpp
@@ -2020,8 +2020,9 @@ void apply_inner_boundary_condition(MeshBlock *pmb,AthenaArray<Real> &prim,Athen
         }
 
         if (std::fabs(prim(IPR,k,j,i)) > 10*std::fabs(p_avg) && !is_in_inner_region ){
-          fprintf(stderr,"Very large P at ijk: %d %d %d \n xyz: %g %g %g \n p: %g P_avg: %g \n prim: %g %g %g %g \n",
-            i,j,k, pmb->pcoord->x1v(i),pmb->pcoord->x2v(j),pmb->pcoord->x3v(k), prim(IPR,k,j,i),p_avg,prim(IDN,k,j,i),prim(IVX,k,j,i),prim(IVY,k,j,i),prim(IVZ,k,j,i));
+          fprintf(stderr,"Very large P at ijk: %d %d %d \n xyz: %g %g %g \n p: %g P_avg: %g \n prim: %g %g %g %g \n gid: %d lid: %g  \n",
+            i,j,k, pmb->pcoord->x1v(i),pmb->pcoord->x2v(j),pmb->pcoord->x3v(k), prim(IPR,k,j,i),p_avg,
+            prim(IDN,k,j,i),prim(IVX,k,j,i),prim(IVY,k,j,i),prim(IVZ,k,j,i), pmb.gid,pmb.lid);
 
 
 
