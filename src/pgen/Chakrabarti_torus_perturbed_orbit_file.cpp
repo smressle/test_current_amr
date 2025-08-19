6467fd2d50f87c1d7207c7c99fa70b2dd04b0757
Sean Ressler <smressle@berkeley.edu>
2025-08-19 12:05:28 -0400

    save

--- src/pgen/Chakrabarti_torus_perturbed_orbit_file.cpp
+++ src/pgen/Chakrabarti_torus_perturbed_orbit_file.cpp
@@ -2244,7 +2244,7 @@ void NobleCooling(MeshBlock *pmb, const Real time, const Real dt,
         // if (L_cool_secondary<0) L_cool_secondary = 0.0;
 
         // L_cool = L_cool/5.0;
-        // L_cool = L_cool * 10;
+        L_cool = L_cool * 10;
 
 
         // L_cool = std::max(L_cool,L_cool_secondary);
