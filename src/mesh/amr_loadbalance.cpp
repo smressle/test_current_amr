4b70097ea50ce700fd9f83a060d9940c7ec108a3
Sean Ressler <smressle@berkeley.edu>
2026-05-25 15:47:01 -0400

    a

--- src/mesh/amr_loadbalance.cpp
+++ src/mesh/amr_loadbalance.cpp
@@ -662,6 +662,7 @@ void Mesh::RedistributeAndRefineMeshBlocks(ParameterInput *pin, int ntot) {
       MeshBlock *pb = FindMeshBlock(n);
             if (MAGNETIC_FIELDS_ENABLED) 
         dummy_bool =pb->pfield->CheckFieldDivergence(pb->pfield->b,"After prolongate f2c");
+        if (dummy_bool) fprintf(stderr,"bad meshblock n: %d on: %g gid: %g \n",n,on,pb->gid);
     }
 
 
