0f9fe998c1447f29bc76e35efd7268c2edb6825d
Sean Ressler <smressle@berkeley.edu>
2026-05-26 16:23:56 -0400

    saveC

--- src/field/ct.cpp
+++ src/field/ct.cpp
@@ -101,12 +101,12 @@ void Field::CT(const Real wght, FaceField &b_out) {
       for (int i=is; i<=ie; ++i) {
         b_out.x3f(k,j,i) -= (wght/area(i))*(len(i+1)*e2(k,j,i+1) - len(i)*e2(k,j,i));
         if (pmb->gid==20311 && i==4 && j==4 && k==12){
-          fprintf(stderr,"Below interface len2: %g %g e2: %g %g \n ",
-            len(i+1), len(i), e2(k,j,i+1), e2(k,j,i));
+          fprintf(stderr,"Below interface len2: %.15e %.15e e2: %.15e %.15e \n wgt: %g area: %g\n ",
+            len(i+1), len(i), e2(k,j,i+1), e2(k,j,i),wght,area(i));
         }
         if (pmb->gid==20315 && i==4 && j==4 && k==4){
-          fprintf(stderr,"above interface len2: %g %g e2: %g %g \n ",
-            len(i+1), len(i), e2(k,j,i+1), e2(k,j,i));
+          fprintf(stderr,"above interface len2: %.15e %.15e e2: %.15e %.15e \n wgt: %g area: %g\n ",
+            len(i+1), len(i), e2(k,j,i+1), e2(k,j,i),wght,area(i));
         }
       }
       if (pmb->block_size.nx2 > 1) {
@@ -117,11 +117,11 @@ void Field::CT(const Real wght, FaceField &b_out) {
           b_out.x3f(k,j,i) +=
               (wght/area(i))*(len_p1(i)*e1(k,j+1,i) - len(i)*e1(k,j,i));
         if (pmb->gid==20311 && i==4 && j==4 && k==12){
-          fprintf(stderr,"Below interface len1: %g %g e1: %g %g \n wgt: %g area: %g \n",
+          fprintf(stderr,"Below interface len1: %.15e %.15e e1: %.15e %.15e \n wgt: %g area: %g \n",
             len_p1(i), len(i), e1(k,j+1,i), e1(k,j,i),wght,area(i));
         }
         if (pmb->gid==20315 && i==4 && j==4 && k==4){
-          fprintf(stderr,"above interface len1: %g %g e1: %g %g \n ",
+          fprintf(stderr,"above interface len1: %.15e %.15e e1: %.15e %.15e \n wgt: %g area: %g\n ",
             len_p1(i), len(i), e1(k,j+1,i), e1(k,j,i),wght,area(i));
         }
         }
