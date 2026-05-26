1b7f7ff681894bc0933bd72a993ce2247c3344ed
Sean Ressler <smressle@berkeley.edu>
2026-05-26 15:53:23 -0400

    save

--- src/field/ct.cpp
+++ src/field/ct.cpp
@@ -100,6 +100,14 @@ void Field::CT(const Real wght, FaceField &b_out) {
 #pragma omp simd
       for (int i=is; i<=ie; ++i) {
         b_out.x3f(k,j,i) -= (wght/area(i))*(len(i+1)*e2(k,j,i+1) - len(i)*e2(k,j,i));
+        if (pmb->gid==20311 && i==4 && j==4 && k==12){
+          fprintf(stderr,"Below interface len2: %g %g e2: %g %g \n ",
+            len(i+1), len(i), e2(k,j,i+1), e2(k,j,i));
+        }
+        if (pmb->gid==20315 && i==4 && j==4 && k==4){
+          fprintf(stderr,"above interface len2: %g %g e2: %g %g \n ",
+            len(i+1), len(i), e2(k,j,i+1), e2(k,j,i));
+        }
       }
       if (pmb->block_size.nx2 > 1) {
         pmb->pcoord->Edge1Length(k,j  ,is,ie,len);
@@ -108,6 +116,14 @@ void Field::CT(const Real wght, FaceField &b_out) {
         for (int i=is; i<=ie; ++i) {
           b_out.x3f(k,j,i) +=
               (wght/area(i))*(len_p1(i)*e1(k,j+1,i) - len(i)*e1(k,j,i));
+        if (pmb->gid==20311 && i==4 && j==4 && k==12){
+          fprintf(stderr,"Below interface len1: %g %g e1: %g %g \n wgt: %g area: %g \n",
+            len_p1(i), len(i), e1(k,j+1,i), e1(k,j,i),wght,area(i));
+        }
+        if (pmb->gid==20315 && i==4 && j==4 && k==4){
+          fprintf(stderr,"above interface len1: %g %g e1: %g %g \n ",
+            len_p1(i), len(i), e1(k,j+1,i), e1(k,j,i),wght,area(i));
+        }
         }
       }
     }
