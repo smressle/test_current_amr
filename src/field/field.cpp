c82fc10b1e26c1d92d8f1ca0059c0a8263507404
Sean Ressler <smressle@berkeley.edu>
2026-05-28 22:27:27 -0400

    savec

--- src/field/field.cpp
+++ src/field/field.cpp
@@ -22,18 +22,18 @@
 #include "field.hpp"
 #include "field_diffusion/field_diffusion.hpp"
 
-#include <cstdint>
-#include <cstring>
+// #include <cstdint>
+// #include <cstring>
 
-bool IsNaN(double x) {
-    uint64_t u;
-    std::memcpy(&u, &x, sizeof(double));
+// bool IsNaN(double x) {
+//     uint64_t u;
+//     std::memcpy(&u, &x, sizeof(double));
 
-    uint64_t exp  = (u >> 52) & 0x7ffULL;
-    uint64_t frac = u & 0xfffffffffffffULL;
+//     uint64_t exp  = (u >> 52) & 0x7ffULL;
+//     uint64_t frac = u & 0xfffffffffffffULL;
 
-    return (exp == 0x7ffULL) && (frac != 0);
-}
+//     return (exp == 0x7ffULL) && (frac != 0);
+// }
 //! constructor, initializes data structures and parameters
 
 Field::Field(MeshBlock *pmb, ParameterInput *pin) :
@@ -359,7 +359,7 @@ bool Field::CheckFieldDivergence(FaceField &b, std::string code_location){
         }  //zm 20 zp: 24)
 
         Real machine_precision = 1e-11; //static_cast<Real>(std::numeric_limits<Real>::epsilon());
-        if (std::fabs(divb)>machine_precision || IsNaN(divb) || !(std::fabs(divb)<machine_precision)) {
+        if (std::fabs(divb)>machine_precision || !(std::fabs(divb)<machine_precision)) {
           bad_divergence=true;
           fprintf(stderr, "nonzero divergence!! at location:  %s\n xyz: %g %g %g x3 faces: %g %g \n divb: %g machine precision: %g  \n ijk: %d %d %d\n face1: %g %g face2: %g %g face3: %g %g \n bx1: %g %g bx2: %g %g bx3: %g %g \n coarse ijk: %d %d %d \n x: %g y: %g zm %g zp: %g \n",
             code_location.c_str(),
