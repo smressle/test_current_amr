9b6503228c011179581bfa79c13e2a56ce4b31c4
Sean Ressler <smressle@berkeley.edu>
2026-05-28 22:18:21 -0400

    iav

--- src/task_list/time_integrator.cpp
+++ src/task_list/time_integrator.cpp
@@ -1690,7 +1690,7 @@ TaskStatus TimeIntegratorTaskList::IntegrateField(MeshBlock *pmb, int stage) {
       pf->CT(stage_wghts[stage-1].beta*pmb->pmy_mesh->dt, pf->b);
 
       // bool dummy_bool = false;
-      // if (MAGNETIC_FIELDS_ENABLED) dummy_bool = pmb->pfield->CheckFieldDivergence(pmb->pfield->b,"After IntegrateField in b");
+      if (MAGNETIC_FIELDS_ENABLED) dummy_bool = pmb->pfield->CheckFieldDivergence(pmb->pfield->b,"After IntegrateField in b");
       // if (MAGNETIC_FIELDS_ENABLED) pmb->pfield->CheckFieldDivergence(pmb->pfield->b1,"After IntegrateField in b1");
 
     }
@@ -1953,11 +1953,11 @@ TaskStatus TimeIntegratorTaskList::UpdateMetric(MeshBlock *pmb, int stage)
   bool dummy_bool;
   if (METRIC_EVOLUTION && pmb->pmy_mesh->update_metric_this_timestep) {
 
-    // if (MAGNETIC_FIELDS_ENABLED) dummy_bool =  pmb->pfield->CheckFieldDivergence(pmb->pfield->b,"Before UpdateMetric in b");
+    if (MAGNETIC_FIELDS_ENABLED) dummy_bool =  pmb->pfield->CheckFieldDivergence(pmb->pfield->b,"Before UpdateMetric in b");
       pmb->pcoord->UpdateUserMetric(t_end_stage,pmb);
       if (pmb->pmy_mesh->multilevel) pmb->pmr->UpdateCoarseMetric(t_end_stage,pmb);
 
-      // if (MAGNETIC_FIELDS_ENABLED) dummy_bool = pmb->pfield->CheckFieldDivergence(pmb->pfield->b,"After UpdateMetric in b");
+      if (MAGNETIC_FIELDS_ENABLED) dummy_bool = pmb->pfield->CheckFieldDivergence(pmb->pfield->b,"After UpdateMetric in b");
       // if (MAGNETIC_FIELDS_ENABLED) pmb->pfield->CheckFieldDivergence(pmb->pfield->b1,"After UpdateMetric in b1");
    }
 
@@ -2100,7 +2100,7 @@ TaskStatus TimeIntegratorTaskList::SetBoundariesField(MeshBlock *pmb, int stage)
   bool dummy_bool;
   if (stage <= nstages) {
     pmb->pfield->fbvar.SetBoundaries();
-    // if (MAGNETIC_FIELDS_ENABLED) dummy_bool = pmb->pfield->CheckFieldDivergence(pmb->pfield->b,"After SetBoundariesField in b");
+    if (MAGNETIC_FIELDS_ENABLED) dummy_bool = pmb->pfield->CheckFieldDivergence(pmb->pfield->b,"After SetBoundariesField in b");
     // if (MAGNETIC_FIELDS_ENABLED) pmb->pfield->CheckFieldDivergence(pmb->pfield->b1,"After SetBoundariesField in b1");
 
     return TaskStatus::success;
@@ -2252,8 +2252,8 @@ TaskStatus TimeIntegratorTaskList::Prolongation(MeshBlock *pmb, int stage) {
     Real dt = (stage_wghts[(stage-1)].beta)*(pmb->pmy_mesh->dt);
     pbval->ProlongateBoundaries(t_end_stage, dt, pmb->pbval->bvars_main_int);
 
-  // bool dummy_bool;
-  // if (MAGNETIC_FIELDS_ENABLED) dummy_bool = pmb->pfield->CheckFieldDivergence(pmb->pfield->b,"After boundary prolongation in b");
+  bool dummy_bool;
+  if (MAGNETIC_FIELDS_ENABLED) dummy_bool = pmb->pfield->CheckFieldDivergence(pmb->pfield->b,"After boundary prolongation in b");
   // if (MAGNETIC_FIELDS_ENABLED) pmb->pfield->CheckFieldDivergence(pmb->pfield->b1,"After boundary prolongation in b1");
 
 
@@ -2289,7 +2289,7 @@ TaskStatus TimeIntegratorTaskList::Primitives(MeshBlock *pmb, int stage) {
                                     ph->w1, pf->bcc, pmb->pcoord,
                                     il, iu, jl, ju, kl, ku);
 
-    // if (MAGNETIC_FIELDS_ENABLED) dummy_bool = pmb->pfield->CheckFieldDivergence(pmb->pfield->b,"After Cons2prim in b");
+    if (MAGNETIC_FIELDS_ENABLED) dummy_bool = pmb->pfield->CheckFieldDivergence(pmb->pfield->b,"After Cons2prim in b");
     // if (MAGNETIC_FIELDS_ENABLED) pmb->pfield->CheckFieldDivergence(pmb->pfield->b1,"After Cons2prim in b1");
     if (pmb->porb->orbital_advection_defined) {
       pmb->porb->ResetOrbitalSystemConversionFlag();
